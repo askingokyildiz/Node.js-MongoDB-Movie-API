e0f06c43f60789cb52c90309b07e03699faf1bc4
Askin Gokyildiz <askin.gokyildiz@puzzledom.local>
2019-10-17 15:39:37 +0300

    İlk post işlemi ile Kayıt yapıldı ve Api Düzeni Readme'ye eklendi

--- helper/db.js
+++ helper/db.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose');
 
 module.exports=()=>{
     mongoose.connect('mongodb+srv://cyberrock:Test.123123123@cyberrock-1qzvt.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-   
+    mongoose.connection.on('error',()=>{
+        console.log('MongoDB: Error!');
+    });
+    mongoose.connection.on('open',()=>{
+        console.log('MongoDB: Connected');
+    });
+    mongoose.Promise=global.Promise;
 };
--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json({status:1});
+//Models
+const Movie=require('../models/Movie');
+
+router.post('/', function(req, res, next) {
+  const movie = new Movie(req.body);
+  /* 
+  movie.save((err, data)=>{
+    if(err)
+    res.json(err);
+    res.json(data);
+  });
+  */
+ const promise =movie.save();
+  promise.then((data)=>{
+    res.json({status:1});
+  }).catch((err)=>{
+    res.json(err);
+  })
+
 });
+
 
 module.exports = router;
